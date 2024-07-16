b17da97dbebd5942e10739bf105d167ab24c4029
muzayenadem <muzynadem@gmail.com>
2024-07-16 13:54:34 +0300

    not fixed

--- server/controller/LandLord/AddProperty.js
+++ server/controller/LandLord/AddProperty.js
@@ -16,31 +16,31 @@ const addProperty = async (req,res) =>{
         let {property} = req.body
         const files = req.files
         property = JSON.parse(property)   
-        const bucket = admin.storage().bucket();    
-        const uploadPromises = files.map(async (file) => {
-            const fileName = `${Date.now()}-${file.originalname}`;
-            const fileRef = bucket.file(`proportyImages/${fileName}`);
+        // const bucket = admin.storage().bucket();    
+        // const uploadPromises = files.map(async (file) => {
+        //     const fileName = `${Date.now()}-${file.originalname}`;
+        //     const fileRef = bucket.file(`proportyImages/${fileName}`);
       
-            const stream = fileRef.createWriteStream({
-              metadata: {
-                contentType: file.mimetype
-              }
-            });
+        //     const stream = fileRef.createWriteStream({
+        //       metadata: {
+        //         contentType: file.mimetype
+        //       }
+        //     });
       
-            await new Promise((resolve, reject) => {
-              stream.on('error', reject);
-              stream.on('finish', resolve);
-              stream.end(file.buffer);
-            });
+        //     await new Promise((resolve, reject) => {
+        //       stream.on('error', reject);
+        //       stream.on('finish', resolve);
+        //       stream.end(file.buffer);
+        //     });
       
-            await fileRef.makePublic();
-            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileRef.name}`;
+        //     await fileRef.makePublic();
+        //     const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileRef.name}`;
 
-            return publicUrl
-          });
+        //     return publicUrl
+        //   });
       
-          const imagesUrl = await Promise.all(uploadPromises);
-          property.images = imagesUrl
+        //   const imagesUrl = await Promise.all(uploadPromises);
+        //   property.images = imagesUrl
         const addedProperty = new propertyModel({
             landLordId:verified.landLordId,
             property
