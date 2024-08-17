7f02c610d193598648066f7b93a77009be89c7ad
muzayenadem <muzynadem@gmail.com>
2024-08-17 11:23:49 +0300

    gtrjjnwt4

--- server/controller/LandLord/landLordSingup.js
+++ server/controller/LandLord/landLordSingup.js
@@ -21,6 +21,7 @@ const landLordSignup = async (req,res) =>{
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password,salt)
 
+
         const newLandLord = new landLordModel({
             firstName,
             lastName,
